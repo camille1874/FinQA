a38148b3e2a18f4273ed770f23e1cbbdce3ecb88
xuzh <15190971357@163.com>
2018-10-11 13:06:41 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -10,20 +10,21 @@ from QACrawler import search_summary
 from stock import FinancialData
 
 from flask import Flask,request,render_template,redirect
-
+from QA1.client import Client
 raw = True
 
 def qa_web():
     app = Flask(__name__)
-    # 绑定访问地址127.0.0.1:5000/user
+    # 绑定访问地址127.0.0.1:5002/qa
     @app.route("/qa", methods=['GET', 'POST'])
     def login():
         if request.method == 'POST':
             global raw
             raw = True
-            ques_type = request.form['chose']
+            #ques_type = request.form['chose']
             question = request.form['question']
-            answer, str_log = find_ans(question, ques_type)
+            #answer, str_log = find_ans(question, ques_type)
+            answer, str_log = find_ans(question)
             if answer[0] == "是":
                 answer = question[0, question.find["是"]] + answer
             elif "为什么" in question:
@@ -34,101 +35,117 @@ def qa_web():
     app.run(host='0.0.0.0', port=5002)
 
 
-def find_ans(question='', ques_type='0'):
+#def find_ans(question='', ques_type='0'):
+def find_ans(question=''):
+    global raw
     log = '答案来源：'
-    try:
-        if ques_type == '1':
-            fd = FinancialData()
-            code = question
-            try:
-                name,table_name_list,table_date_list,url_list = fd.get_informations(fd.cwnb + code + '.html')
-            except Exception as e:
-                name,table_name_list,table_date_list,url_list = fd.get_informations(fd.cwnb1 + code + '.html')
-            return(fd.get_result(name,code,table_name_list,table_date_list,url_list), log)
-        elif ques_type == '0':
-            input_message = question
-            if len(input_message) > 60:
-                return (mybot.respond("句子长度过长"), log)
-            elif input_message.strip() == '':
-                return (mybot.respond("无"), log)
-        
-            message = T.wordSegment(input_message)
-            words = T.postag(input_message)
-        
-        
-            if message == 'q':
-                exit()
+    cnt = 0
+    #try:
+            # 判断提问股票代码还是金融常识
+            #if ques_type == '1':
+            #    fd = FinancialData()
+            #    code = question
+            #    try:
+            #        name,table_name_list,table_date_list,url_list = fd.get_informations(fd.cwnb + code + '.html')
+            #    except Exception as e:
+            #        name,table_name_list,table_date_list,url_list = fd.get_informations(fd.cwnb1 + code + '.html')
+            #    return(fd.get_result(name,code,table_name_list,table_date_list,url_list), log)
+            #elif ques_type == '0':
+    input_message = question
+    if len(input_message) > 60:
+        return (mybot.respond("句子长度过长"), log)
+    elif input_message.strip() == '':
+        return (mybot.respond("无"), log)
+    
+    # 检索本地知识库得到答案，代码在QA1文件夹
+    cnt += 1
+    log += str(cnt) + ':检索本地知识库\n'
+    old_client = Client()
+    ans, log_tmp = old_client.qa_find_ans(input_message)
+    log += log_tmp
+    if ans != "不知道~":
+        return (clean_str(ans), log)        
+    log += '本地知识库找不到答案或答案不确定\n' 
+    
+
+
+    # 借助百度和Bing得到答案
+    message = T.wordSegment(input_message)
+    words = T.postag(input_message)
+    
+    response = mybot.respond(message)
+    #log += 'AIML模板返回内容:' + response + '\n'
+    if response == "":
+        ans = mybot.respond('不知道~')
+        return (ans, log)
+    elif response[0] == '#':
+        cnt += 1
+        log += str(cnt) + ':匹配不到问句模板\n'
+        if response.__contains__("searchbaike"):
+            res = response.split(':')
+            entity = str(res[1]).replace(" ","")
+            attr = str(res[2]).replace(" ","")
+            cnt += 1
+            log += str(cnt) + ':匹配到实体属性模板,' + '实体:' + entity + ' 属性:' + attr + '\n'
+            ans = baike.query(entity, attr)
+            if type(ans) == list:
+                cnt += 1
+                log += str(cnt) + ':来自百科Infobox\n'
+                return (QAT.ptranswer(ans,False), log)
+            elif '-找不到' in ans:
+                cnt += 1
+                log += str(cnt) + ':百科Infobox查询不到:' + ans + '\n'
+                cnt += 1
+                log += str(cnt) + '来自搜索\n'
+                (ans, tmplog) = search_summary.kwquery(input_message)
+                log += tmplog                    
+
+        elif response.__contains__("NoMatchingTemplate"):
+            cnt += 1
+            log += str(cnt) + ':匹配不到实体关系模板\n'
+            cnt += 1
+            log += str(cnt) + ':来自搜索\n'
+            (ans,tmplog) = search_summary.kwquery(input_message)
+            log += tmplog
+
+        if len(ans) == 0:
+            cnt += 1
+            log += str(cnt) + ':未查询到答案\n'
+            return (mybot.respond('不知道~'), log)
+
+        elif len(ans) >1:
+            cnt += 1
+            log += str(cnt) + ':返回百度摘要\n'
+            if raw == False and ('什么是' in question or '是什么' in question):
+                result = "给你找到几篇新闻："
+                for a in ans:
+                    result += a + '\n'
+                return (result, log)
             else:
-                response = mybot.respond(message)
-        
-                if response == "":
-                    ans = mybot.respond('不知道~')
-                    return (ans, log)
-                # 百科搜索
-                elif response[0] == '#':
-                    # 匹配百科
-                    if response.__contains__("searchbaike"):
-                        #print "searchbaike"
-                        #print (response)
-                        res = response.split(':')
-                        #实体
-                        entity = str(res[1]).replace(" ","")
-                        #属性
-                        attr = str(res[2]).replace(" ","")
-                        log += '实体：' + entity + ' 属性：' + attr + '\n'
-        
-                        ans = baike.query(entity, attr)
-                        # 命中答案
-                        if type(ans) == list:
-                            log += '来自百科图谱\n'
-                            return (QAT.ptranswer(ans,False), log)
-                        #elif ans.decode('utf-8').__contains__(u'::找不到'):
-                        elif '::找不到' in ans:
-                            #百度+Bing
-                            log += '百科图谱查询不到\n'
-                            log += '来自百科摘要+Bing摘要\n'
-                            ans = search_summary.kwquery(input_message)
-        
-                    # 匹配不到模版，通用查询
-                    elif response.__contains__("NoMatchingTemplate"):
-                        log += '匹配不到模板\n来自百科摘要+Bing摘要\n'
-                        ans = search_summary.kwquery(input_message)
-                      
-        
-                    if len(ans) == 0:
-                        log += '未查询到答案\n'
-                        return (mybot.respond('不知道~'), log)
-                    elif len(ans) >1:
-                        log += '返回多个答案\n'
-                        global raw
-                        if raw == False and ('什么是' in question or '是什么' in question):
-                            result = "相关内容："
-                            for a in ans:
-                                result += clean_str(a)
-                                return (result, log)
-                        else:
-                            raw = False
-                            question = question.replace("是什么", "").replace("什么是", "")
-                            ans1, log1 = find_ans("什么是" + question) 
-                            ans2, log2 = find_ans(question + "是什么")
-                            if "相关内容" not in ans1:
-                                return (ans1, log1)
-                            else:
-                                return (ans2, log2)
-                    else:
-                        return (clean_str(ans[0]), log)
-                # 匹配模版
+                raw = False
+                question = question.replace("是什么", "").replace("什么是", "")
+                ans2, log2 = find_ans(question + "是什么")
+                ans1, log1 = find_ans("什么是" + question) 
+                if "给你找到几篇新闻" not in ans1:
+                    return (ans1, log1)
                 else:
-                    log += '匹配问句模板\n'
-                    return (clean_str(response), log)
-    except Exception as e:
-        print(e)
-        return ("换个问题试试~", log)    
+                    return (ans2, log2)
+        else:
+            return (clean_str(ans[0]), log)
+
+
+
+    # 直接匹配问句模版
+    else:
+        cnt += 1
+        log += str(cnt) + ':匹配问句模板\n'
+        return (clean_str(response), log)
+    #except Exception as e:
+    #    print(e)
+    #    return ("换个问题试试~", log)    
 
 
 if __name__ == '__main__':
-
-    #初始化jb分词器
     T.jieba_initialize()
 
     mybot_path = './'
@@ -136,14 +153,10 @@ if __name__ == '__main__':
 
     mybot = aiml.Kernel()
     mybot.learn(os.path.split(os.path.realpath(__file__))[0]+"/resources/std-startup.xml")
-    mybot.learn(os.path.split(os.path.realpath(__file__))[0] + "/resources/bye.aiml")
     mybot.learn(os.path.split(os.path.realpath(__file__))[0] + "/resources/tools.aiml")
-    mybot.learn(os.path.split(os.path.realpath(__file__))[0] + "/resources/funny.aiml")
-    mybot.learn(os.path.split(os.path.realpath(__file__))[0] + "/resources/OrdinaryQuestion.aiml")
-    mybot.learn(os.path.split(os.path.realpath(__file__))[0] + "/resources/Common conversation.aiml")
-
-    # mybot.respond('Load Doc Snake')
-    #载入百科属性列表
+    mybot.learn(os.path.split(os.path.realpath(__file__))[0] + "/resources/xiaohuangji.aiml")
+    mybot.learn(os.path.split(os.path.realpath(__file__))[0] + "/resources/ordinary_question.aiml")
+    mybot.learn(os.path.split(os.path.realpath(__file__))[0] + "/resources/common_conversation.aiml")
 
     qa_web()    
 
